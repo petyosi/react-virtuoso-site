af6f1e7e0d6920a3485eef792de5de84ab0af5b6
Petyo Ivanov <underlog@gmail.com>
2020-02-11 15:57:06 +0200

    Fix broken example

--- src/examples/PrependItems.js
+++ src/examples/PrependItems.js
@@ -1,22 +1,37 @@
-import React, { useState, useRef } from 'React'
+import React, { useState, useRef, useCallback } from 'react'
 import { Virtuoso } from 'react-virtuoso'
 import { getUser } from './FakeData'
 import { UserItem } from './ExampleComponents'
 
 export default () => {
   const virtuoso = useRef(null)
-  const initialIndexOffset = React.useRef(10000)
+  const initialIndexOffset = useRef(10000)
   const [users, setUsers] = useState(
     Array(200)
       .fill(true)
-      .map((_, index) => getUser(initialIndexOffset.current + index))
+      .map((_, index) => getUser(10000 + index))
   )
+
+  const prependItems = useCallback(() => {
+    const usersToPrepend = 100
+    initialIndexOffset.current -= usersToPrepend
+    setUsers([
+      ...Array(usersToPrepend)
+        .fill(true)
+        .map((_, index) =>
+          getUser(initialIndexOffset.current + index)
+        ),
+      ...users,
+    ])
+    virtuoso.current.adjustForPrependedItems(usersToPrepend)
+    return false
+  }, [initialIndexOffset, users, setUsers])
+
   return (
     <div style={{ display: 'flex' }}>
       <div>
         <Virtuoso
           totalCount={users.length}
-          ref={virtuoso}
           item={index => (
             <UserItem user={users[index]} index={index} />
           )}
@@ -26,26 +41,7 @@ export default () => {
       <div>
         <ul className="knobs">
           <li>
-            <button
-              onClick={() => {
-                const usersToPrepend = 100
-                initialIndexOffset.current -= usersToPrepend
-                setUsers([
-                  ...Array(usersToPrepend)
-                    .fill(true)
-                    .map((_, index) =>
-                      getUser(initialIndexOffset.current + index)
-                    ),
-                  ...users,
-                ])
-                virtuoso.current.adjustForPrependedItems(
-                  usersToPrepend
-                )
-                return false
-              }}
-            >
-              Prepend 100 items
-            </button>
+            <button onClick={prependItems}>Prepend 100 items</button>
           </li>
         </ul>
       </div>
